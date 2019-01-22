a2f08f4ebe43d345e3bd4c3a992741726e9b4bbd
mifyang <mifyang@126.com>
2019-01-22 19:34:19 +0800

    增加判断是否是文件或文件夹

--- CheckFileDate.py
+++ CheckFileDate.py
@@ -6,4 +6,7 @@ print(dirlist)
 for file in dirlist:
     file_mtime = os.stat(path + file).st_mtime
     f_mtime = datetime.datetime.fromtimestamp(file_mtime).strftime('%Y-%m-%d')
-    print("%s 最后修改时间是: %s" %(file, f_mtime))
+    if os.path.isdir(path + file):
+        print("%s 文件夹\n最后修改时间是: %s" %(file, f_mtime))
+    if os.path.isfile(path + file):
+        print("%s 文件\n最后修改时间是: %s" %(file, f_mtime))
