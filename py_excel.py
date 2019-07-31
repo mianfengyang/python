04cf33e392c2d24a91b77f6433b931649d9a3801
zhangs <xxx@126.com>
2019-07-31 18:13:03 +0800

    调试向单元格写入公式 写法比较难以接受 不直观

--- py_excel.py
+++ py_excel.py
@@ -1,1 +1,21 @@
-import openpyxl
+from openpyxl import Workbook
+import datetime
+wb = Workbook()
+ws = wb.active
+ws['A1'] = "用户目录"
+ws['B1'] = "最新修改目录"
+ws['C1'] = "最新修改时间"
+ws['D1'] = "用户"
+ws['E1'] = "备份状态"
+ws['F1'] = "备注"
+
+# print(datetime.datetime.now())
+
+ws['c2'] = datetime.datetime.now()
+# ws['E2'] = '=IF(AND(YEAR(C2)=2019,MONTH(C2)=7),"已备份","未备份")'
+
+for x in range(2, 24):
+    ws.cell(row=x, column=5).value = "=IF(AND(YEAR(" + "C" + str(x) + ")=2019,MONTH(" + "C" + str(x) + ")=7)," + "\"已备份\"" + "," + "\"未备份\"" +")"
+
+
+wb.save(r'//192.168.1.11/pubin/杨绵峰/工作文件/备份检查/testFunc.xlsx')
