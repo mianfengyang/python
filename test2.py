827dd78562947fbe89481f5658913eedb531d6fd
zhangs <xxx@126.com>
2019-08-30 14:46:17 +0800

    lambda

--- test2.py
+++ test2.py
@@ -4,6 +4,8 @@
 import queue
 list1 = ['aa', 'bb', 'cc', 'dd']
 list2 = ['11', '22', '33', '44']
+a = [1, 2, 3, 4]
+b = {}
 q = queue.Queue()
 
 def add_value():
@@ -16,8 +18,15 @@ def get_value():
 
 
 def main():
-    add_value()
-    get_value()
+    # add_value()
+    # get_value()
+    global b
+    for i in range(len(a) - 1):
+        if a[i] > a[i+1]:
+            b['tt'] = a[i]
+        else:
+            b['tt'] = a[i+1]
+    print(b)
 
 if __name__ == "__main__":
     main()
