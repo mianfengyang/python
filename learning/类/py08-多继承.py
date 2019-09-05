519f7afab7e77fd56fee3ee99b3f6443b0b593e9
zhangs <xxx@126.com>
2019-09-05 13:14:30 +0800

    多层多继承

--- learning/类/py08-多继承.py
+++ learning/类/py08-多继承.py
@@ -22,7 +22,7 @@ def main():
             self.kongfu = '[古法煎饼果子配方]'
 
         def make_cake(self):
-            print(f'运用{self.kongfu} 制作煎饼果子')
+            print(f'运用 {self.kongfu} 制作煎饼果子')
 
     class School(object):
         def __init__(self):
@@ -32,11 +32,30 @@ def main():
             print(f'运用 {self.kongfu} 制作煎饼果子')
 
     class Prentice(School, Master):
+        def __init__(self):
+            self.kongfu = '[独创煎饼果子配方]'
+
+        def make_cake(self):
+            # 加自已的初始化原因: 如果不加，则会自动调用上一次调用过的父类同名属性
+            self.__init__()
+            print(f'运用 {self.kongfu} 制作煎饼果子')
+
+        def Master_make_cake(self):
+            Master.__init__(self)
+            Master.make_cake(self)
+
+        def School_make_cake(self):
+            School.__init__(self)
+            School.make_cake(self)
+
+    class TuSun(Prentice):
         pass
 
-    daqiu = Prentice()
-    print(daqiu.kongfu)
-    daqiu.make_cake()
+    xiaoqiu = TuSun()
+    xiaoqiu.make_cake()
+    xiaoqiu.Master_make_cake()
+    xiaoqiu.School_make_cake()
+    xiaoqiu.make_cake()
 
 
 if __name__ == '__main__':
