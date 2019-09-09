0b1f9f40f190e418f20b9660cdb3839511acd5be
mifyang <mifyang@126.com>
2019-09-09 11:02:23 +0800

    模块和包

---
+++ learning/模块和包/mypackage/mymodule1.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+
+"""
+=============================================================
+#   project: python
+#      file: mymodule1.py
+#    author: mianfeng.yang
+#      date: 2019-09-09 10:17:49
+=============================================================
+"""
+
+
+def info_print1():
+    print('mymodule1')
+
+def main():
+    """函数说明文档 写在这里"""
+    pass
+
+
+if __name__ == '__main__':
+    pass
