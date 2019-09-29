cf3501e0dc6006835279a708b0a6cad97f793224
mifyang <mifyang@126.com>
2019-09-29 18:06:48 +0800

    新增爬虫文件夹

---
+++ learning/爬虫/py01-requests-1.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+
+"""
+=============================================================
+#   project: python
+#      file: py01-requests-1.py
+#    author: mianfeng.yang
+#      date: 2019-09-27 17:30:58
+=============================================================
+"""
+import requests
+if __name__ == '__main__':
+    pass
