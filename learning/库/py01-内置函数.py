604770b85b971437924bb93348bff59f5508b1cf
mianfengyang <181500944@qq.com>
2020-02-26 13:56:02 +0800

    qq

---
+++ learning/库/py01-内置函数.py
@@ -0,0 +1,33 @@
+#coding=utf-8
+
+"""
+=============================================================
+#   project: python
+#      file: py01-内置函数.py
+#    author: mianfeng.yang
+#      date: 2019-10-08 15:53:41
+=============================================================
+
+内置函数
+abs() delattr() hash() memoryview() set()
+all() dict() help() min() setattr()
+any() dir() hex() next() slice()
+ascii() divmod() id() object() sorted()
+bin() enumerate() input() oct() staticmethod()
+bool() eval() int() open() str()
+breakpoint() exec() isinstance() ord() sum()
+bytearray() filter() issubclass() pow() super()
+bytes() float() iter() print() tuple()
+callable() format() len() property() type()
+chr() frozenset() list() range() vars()
+classmethod() getattr() locals() repr() zip()
+compile() globals() map() reversed() __import__()
+complex() hasattr() max() round()
+"""
+str_a = 'a'
+str_a.center(50,"#")
+print(str_a.center(50,"#"))
+
+
+if __name__ == '__main__':
+    pass
