580f3f32c58a9a837feff3c8ecd28b6f183712d7
frank <frank@126.com>
2023-11-02 11:39:44 +0800

    修改了程序逻辑： 上一次的版本中存在一个BUG，当日期中的天数不为1号且请求码不是200时，月份仍被设置为上个月。 此次提交修复了这个BUG。

--- learning/spider/frn_vpn/get_clash_frn.py
+++ learning/spider/frn_vpn/get_clash_frn.py
@@ -43,18 +43,22 @@ class UpFreeNode:
         self.req = requests.get(self.downloadUrl)
         print("Curday url is " + str(self.req.status_code))
         if self.req.status_code != 200:
-            if self.day == "01" and self.curMonth in self.bigMon:
-                self.day = "31"
-            if self.day == "01" and self.curMonth in self.smallMon:
+            if self.curMonth in self.bigMon and self.curDay == "01":
                 self.day = "30"
-            if self.curMonth == "03" and int(self.curYear) % 4 == 0:
+            if self.curMonth in self.smallMon and self.curDay == "01":
+                self.day = "31"
+            if self.curMonth == "03" and int(self.curYear) % 4 == 0 and self.curDay == "01":
                 self.day = "29"
             else:
                 self.day == "28"
-            if int(self.curMonth) < 10:
+            if int(self.curMonth) < 10 and self.curDay == "01":
                 self.curMonth = "0" + str(int(self.curMonth) - 1)
             else:
-                self.curMonth = str(int(self.curMonth) - 1)
+                self.curMonth = self.curMonth
+            if int(self.curDay) < 10:
+                self.day = "0" + str(int(self.curDay) - 1)
+            else:
+                self.day = str(int(self.curDay) - 1)
             self.downloadUrl = self.baseUrl + self.curYear + '/' + self.curMonth + '/' + self.curMonth + self.day + '.yaml'
         else:
             self.downloadUrl = self.downloadUrl
@@ -76,7 +80,7 @@ class UpFreeNode:
 
     def filterYml(self):
         lineList = []
-        matchPattern_filters = re.compile(r'香港|台湾|HK|中国|CN|JP|日本|法国|新加坡')
+        matchPattern_filters = re.compile(r'香港|台湾|HK|中国|CN|JP|日本|法国|新加坡|德国')
         matchPattern_dns = re.compile(r'119.29.29.29')
         file = open(self.fs_yml,"r",encoding='UTF-8')
         while 1:
