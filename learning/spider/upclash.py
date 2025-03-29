1803808872ccd07f210ee55e1571cc4faa3ae4a7
mianfengyang <181500944@qq.com>
2025-03-29 16:48:02 +0800

    move to github

--- learning/spider/upclash.py
+++ learning/spider/upclash.py
@@ -3,7 +3,8 @@ import os
 import subprocess
 import platform
 import datetime
-
+import requests
+import certifi
 import httpx
 from fake_useragent import UserAgent
 from lxml import etree
@@ -19,18 +20,13 @@ class UpFreeNode:
         self.curMonth = datetime.datetime.today().strftime('%m')
         self.curDay = datetime.datetime.today().strftime('%d')
         self.baseUrl = "https://www.freeclashnode.com/uploads/"
-        self.backUrl = self.curYear + '/' + self.curMonth + '/' + '1-' + self.curYear + self.curMonth + self.curDay + '.yaml'
+        self.backUrl = self.curYear + '/' + self.curMonth + '/' + '3-' + self.curYear + self.curMonth + self.curDay + '.yaml'
         self.downloadUrl = None
-        self.targetdir = "/data/project/cfvpn/"
-        self.sourcedir = "/home/frank/.local/share/io.github.clash-verge-rev.clash-verge-rev/"
-        self.sfile = "frn.yaml"
-        self.sourcefile = self.sourcedir + self.sfile
         
         self.ua = UserAgent()
         self.headers = {'User-Agent':self.ua.random}
         self.proxies = {'http://': 'http://127.0.0.1:7899','https://': 'http://127.0.0.1:7899'}
 
-        os.chdir("/data/project/python/learning/spider")
 
 
     def getDownloadUrl(self):
@@ -42,7 +38,7 @@ class UpFreeNode:
         else:
             self.day = str(self.day)
         self.downloadUrl = self.baseUrl + self.backUrl
-        self.req = httpx.get(self.downloadUrl,headers=self.headers,verify=False)
+        self.req = requests.get(url=self.downloadUrl,headers=self.headers,verify=False,timeout=None,proxies=None)
         if self.req.status_code != 200:
             if self.curMonth in self.bigMon and self.curDay == "01":
                 self.day = "30"
@@ -60,41 +56,21 @@ class UpFreeNode:
                 self.day = "0" + str(int(self.curDay) - 1)
             else:
                 self.day = str(int(self.curDay) - 1)
-            self.backUrl = self.curYear + '/' + self.curMonth + '/' + '1-' + self.curYear + self.curMonth + self.day + '.yaml'
+            self.backUrl = self.curYear + '/' + self.curMonth + '/' + '3-' + self.curYear + self.curMonth + self.day + '.yaml'
             self.downloadUrl = self.baseUrl + self.backUrl
         else:
             self.downloadUrl = self.downloadUrl
         return self.downloadUrl
     
     def getYamlByRequests(self):
-        try:
-            self.downloadUrl = self.getDownloadUrl()
-        except Exception as e:
-            print(f"can not get downloadUrl, {e}")
-        else:
-            print(f"DownloadUrl is {self.downloadUrl}")
-            if "v2rayse" in self.downloadUrl:
-                self.req = httpx.get(self.downloadUrl,headers=self.headers,proxies=self.proxies)
-            else:
-                self.req = httpx.get(self.downloadUrl,verify=False,headers=self.headers)
-            self.req.encoding = 'utf-8'
-            self.req = self.req.text
-            with open(self.fs_yml,"w",encoding="utf-8") as file:
-                file.write(self.req)
-            print(f"Download success and wirte to file {self.sfile}")
-
-    def gitAddCommit(self):
-        copy = subprocess.run(['cp', self.sourcefile,self.targetdir],text=True,capture_output=True)
-        if copy.returncode == 0:
-            print(f"copy file {self.sfile} to {self.targetdir} success")
-        os.chdir(self.targetdir)
-        gitadd = subprocess.run(['git', 'add', self.sfile],text=True,capture_output=True)
-        if gitadd.returncode == 0:
-            print(f"git add {self.sfile}")
-        comit = "update " + self.sfile + ' ' + datetime.datetime.today().strftime("%Y-%m-%d")
-        gitcomit = subprocess.run(['git', 'commit', '-m',comit],text=True,capture_output=True)
-        if gitcomit.returncode == 0:
-            print(f"git comit {comit} success")
+        self.downloadUrl = self.getDownloadUrl()
+        print(f"DownloadUrl is {self.downloadUrl}")
+        self.req = requests.get(url=self.downloadUrl,headers=self.headers,verify=False,timeout=None,proxies=None)
+        self.req.encoding = 'utf-8'
+        self.req = self.req.text
+        with open(self.fs_yml,"w",encoding="utf-8") as file:
+            file.write(self.req)
+        print(f"Write file {self.fs_yml} success")
 
     def filterYml(self):
         lineList = []
@@ -129,47 +105,27 @@ class UpFreeNode:
     def run(self):
         print(f"=============================== Up {type(self).__name__} ===============================")
         self.getYamlByRequests()
-        self.gitAddCommit()
+        #self.gitAddCommit()
 
-class UpOpenrunner(UpFreeNode):
-    def __init__(self, fs_yml) -> None:
-        super().__init__(fs_yml)
-        self.baseUrl = "https://freenode.openrunner.net/uploads/"
-        self.backUrl = self.curYear + self.curMonth + self.curDay + '-clash.yaml'
-        self.sfile = "opr.yaml"
-        self.sourcefile = self.sourcedir + self.sfile
 
 class UpChangfen(UpFreeNode):
     def __init__(self, fs_yml) -> None:
         super().__init__(fs_yml)
         self.baseUrl = "https://www.cfmem.com/"
-        self.sfile = "cf.yaml"
-        self.sourcefile = self.sourcedir + self.sfile
-        self.testfile = "/data/project/python/learning/spider/cf.html"
 
     def getDownloadUrl(self):
-        try:
-            self.req = httpx.get(self.baseUrl,headers=self.headers,proxies=self.proxies)
-        except Exception as e:
-            print(f"Query {self.baseUrl} failed {e}")
-        else:
-            self.html = self.req.text
-            #print(html)
-            with open(self.testfile,"w",encoding="utf-8") as cfhtml:
-                cfhtml.write(self.html)
-            try:
-                self.text_find = etree.HTML(self.html)
-                self.cur_url = self.text_find.xpath('(//h2/a[contains(@title,"2024")])[1]/@href')[0]
-                #print(cur_url)
-            except Exception as e:
-                print(f"Invalid url check xpath error code: {e}")
-            else:
-                self.next_req = httpx.get(self.cur_url)
-                self.html = self.next_req.text
-                self.text_find = etree.HTML(self.html)
-                #print(text_find.text())
-                self.downloadUrl = self.text_find.xpath('(//span[contains(.,"mihomo")])[1]/text()')[0]
-                self.downloadUrl = re.split(">",self.downloadUrl)[-1].lstrip()
+        self.req = requests.get(url=self.baseUrl,headers=self.headers,verify=False,timeout=None,proxies=None)
+        self.html = self.req.text
+        self.text_find = etree.HTML(self.html)
+        self.cur_url = self.text_find.xpath('(//h2/a/@href)[1]')[0]
+        print(self.cur_url)
+        self.next_req = requests.get(url=self.cur_url,headers=self.headers,verify=False,timeout=None,proxies=None)
+        self.html = self.next_req.text
+        self.text_find = etree.HTML(self.html)
+        #print(text_find.text())
+        self.downloadUrl = self.text_find.xpath('//span[contains(.,"mihomo")]/text()')[0]
+        self.downloadUrl = re.split(">",self.downloadUrl)[-1].lstrip()
+        #print(self.downloadUrl)
         return self.downloadUrl
 
 def gitPush():
@@ -184,39 +140,26 @@ def upcf(fs):
 def upfrn(fs):
     frn = UpFreeNode(fs)
     frn.run()
-    
-def upopr(fs):
-    opr = UpOpenrunner(fs)
-    opr.run()
-
-def upAll(ins,fs):
-    t = ins(fs)
-    t.run()
 
 
 if __name__ == "__main__":
-    fs_L_c4w_frn = "/home/frank/.config/clash/freenode.yaml"
-    fs_L_c4w_opr = "/home/frank/.config/clash/openrunner.yaml"
-    fs_L_c4w_cf = "/home/frank/.config/clash/changfen.yaml"
-    fs_L_verge_opr = "/home/frank/.local/share/io.github.clash-verge-rev.clash-verge-rev/opr.yaml"
     fs_L_verge_frn = "/home/frank/.local/share/io.github.clash-verge-rev.clash-verge-rev/frn.yaml"
     fs_L_verge_cf = "/home/frank/.local/share/io.github.clash-verge-rev.clash-verge-rev/cf.yaml"
-    fs_W_yml = "D:\\project\\python\\learning\\spider\\frn_vpn\\frn.yml"
-    fd_L_c4w_frn = "/home/frank/.config/clash/profiles/1722600464996.yml"
-    fd_L_c4w_opr = "/home/frank/.config/clash/profiles/1721976735187.yml"
-    fd_W_yml = "C:\\Users\\frank\\.config\\clash\\profiles\\1693528527309.yml"
+    fs_W_verge_cf = "C:\\Users\\frandon\\AppData\\Roaming\\io.github.clash-verge-rev.clash-verge-rev\\profiles\\LpjWCKEUn4PN.yaml"
+    fs_W_verge_frn = "C:\\Users\\frandon\\AppData\\Roaming\\io.github.clash-verge-rev.clash-verge-rev\\profiles\\Lva8saf6iNxY.yaml"
+
     
     if platform.system() == "Linux":
         tfrn = Thread(target=upfrn,args=(fs_L_verge_frn,))
-        topr = Thread(target=upopr,args=(fs_L_verge_opr,))
         tcf = Thread(target=upcf,args=(fs_L_verge_cf,))
+        tfrn.start()
+        tfrn.join()
+        tcf.start()
+        tcf.join()
     if platform.system() == "Windows":
-        frn = UpFreeNode(fs_W_yml) 
-    tfrn.start()
-    tfrn.join()
-    topr.start()
-    topr.join()
-    tcf.start()
-    tcf.join()
-    gitPush()
-    
+        tfrn = Thread(target=upfrn,args=(fs_W_verge_frn,))
+        tcf = Thread(target=upcf,args=(fs_W_verge_cf,))
+        tfrn.start()
+        tfrn.join()
+        tcf.start()
+        tcf.join()
